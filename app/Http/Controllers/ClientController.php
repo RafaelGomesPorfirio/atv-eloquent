3f081f2cae5e2e5b67f934509c4002b17d9e3f73
Rafael <rafagomes475@gmail.com>
2023-01-18 16:17:15 -0300

    atl

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bill;
 use App\Models\Client;
 use Illuminate\Http\Request;
 
@@ -35,5 +36,11 @@ class ClientController extends Controller
         $client = Client::where('name', 'LIKE', "%{$text}%")->get();
 
         return response()->json($client);
+    }
+
+    public function getBills($client) {
+        $bills = Bill::where('client_id', $client)->get();
+
+        return response()->json($bills);
     }
 }
